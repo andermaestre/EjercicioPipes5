3f2e14527a179b28150c4133c0ca0050996a116d
Ander Maestre <andermaestre_90@hotmail.es>
2019-10-16 19:49:34 +0200

    polla

---
+++ EjerciciosPipes5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace EjerciciosPipes5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int i = 0; i < 2; i++)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                using (AnonymousPipeServerStream apcs = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable))
+                {
+                    using (StreamWriter sw = new StreamWriter(apcs))
+                    {
+                        Process p = new Process();
+                        psi.FileName = "..\\..\\..\\Hijo\\bin\\Debug\\Hijo.exe";
+                        psi.UseShellExecute = false;
+                        psi.Arguments = apcs.GetClientHandleAsString();
+                        p.StartInfo = psi;
+
+                        p.Start();
+                        sw.WriteLine(i);
+                        
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hijo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Hijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont;
+            using (AnonymousPipeClientStream pipe = new AnonymousPipeClientStream(PipeDirection.In, args[0]))
+            {
+
+                using (StreamReader sr = new StreamReader(pipe))
+                {
+                    cont = int.Parse(sr.ReadLine());
+                }
+            }
+
+            if (cont == 0)     //Hijo 1
+            {
+                String aps;
+                Console.WriteLine("Escribeme algo.");
+                using (NamedPipeClientStream pipa = new NamedPipeClientStream(".", "pepepipe", PipeDirection.Out))
+                {
+
+                    pipa.Connect();
+
+                    using (StreamWriter sw = new StreamWriter(pipa))
+                    {
+                        sw.AutoFlush = true;
+                        do
+                        {
+                            
+                            aps = Console.ReadLine();
+                            sw.WriteLine(aps);
+                        } while (!aps.ToUpper().ToString().Equals("FIN"));
+                    }
+
+                }
+            }
+            else              //Hijo 2
+            {
+                using (NamedPipeServerStream pipa = new NamedPipeServerStream("pepepipe", PipeDirection.In))
+                {
+                    pipa.WaitForConnection();
+
+                    using (StreamReader sr = new StreamReader(pipa))
+                    {
+
+                        String a;
+                        do
+                        {
+                            a = sr.ReadLine();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(a);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        } while (!a.ToUpper().ToString().Equals("FIN"));
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+
